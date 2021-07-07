c6a304fd13847837cd445aaf1ec03c1de5e02492
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-07-07 18:57:14 -0300

    Create Calculadora.java

---
+++ testes-unitarios/src/main/java/br/com/alura/tdd/modelo/Calculadora.java
@@ -0,0 +1,18 @@
+package br.com.alura.tdd.modelo;
+
+public class Calculadora {
+		
+	public Calculadora() {
+	}
+
+
+
+	public int soma(int n1, int n2) {
+		
+		
+		int soma = n1 + n2;
+		
+		return soma;
+	}
+
+}
