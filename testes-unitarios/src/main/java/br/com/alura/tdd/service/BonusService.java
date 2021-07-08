0f194a696e4c61a9bfc2069ff011f35d126523a3
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-07-08 10:09:10 -0300

    Update BonusService.java

--- testes-unitarios/src/main/java/br/com/alura/tdd/service/BonusService.java
+++ testes-unitarios/src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -8,8 +8,9 @@ public class BonusService {
 
 	public BigDecimal calcularBonus(Funcionario funcionario) {
 		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
+		
 		if (valor.compareTo(new BigDecimal("1000")) > 0) {
-			valor = BigDecimal.ZERO;
+			throw new IllegalArgumentException("Funcionario com salário superior ao planejado.");
 		}
 		return valor;
 	}
