ed86ed9038dbbc83efd7ca226a7939ed7e6572e3
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-07-08 10:09:22 -0300

    Update ReajusteServiceTest.java

--- testes-unitarios/src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
+++ testes-unitarios/src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.tdd.modelo.Desempenho;
@@ -11,33 +12,30 @@ import br.com.alura.tdd.modelo.Funcionario;
 
 public class ReajusteServiceTest {
 	
+	private ReajusteService service;
+	private Funcionario funcionario;
+	
+	@BeforeEach
+	public void inicializar() {
+		this.service = new ReajusteService();
+		this.funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000.0"));
+	}
+
 	@Test
 	public void reajusteADesejar() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000.0"));
-		
 		service.concederReajuste(funcionario, Desempenho.A_DESEJAR);
-		
 		Assert.assertEquals(new BigDecimal("1030.00"),funcionario.getSalario());
 	}
 	
 	@Test
 	public void reajusteBom() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000.0"));
-		
 		service.concederReajuste(funcionario, Desempenho.BOM);
-		
 		Assert.assertEquals(new BigDecimal("1150.00"),funcionario.getSalario());
 	}
 	
 	@Test
 	public void reajusteOtimo() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000.0"));
-		
 		service.concederReajuste(funcionario, Desempenho.OTIMO);
-		
 		Assert.assertEquals(new BigDecimal("1200.00"),funcionario.getSalario());
 	}
 
